940eb9f9789153846e1ff7e21713b75f7d33e440
Alma G <almarcianita@gmail.com>
2018-03-06 17:48:16 -0600

    Ejercicio parte 1

---
+++ UTesting/UTesting/Homework.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace UTesting
+{
+    public class Homework : IHomework
+    {
+        public string ValidateEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ValidateFibonnacci(string list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ValidatePassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ValidatePhone(string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ValidateTime(string time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
