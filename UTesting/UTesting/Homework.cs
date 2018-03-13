06d8cbaf1862fb8c9f2afc0e8f51eaf29947c250
Alma G <almarcianita@gmail.com>
2018-03-12 18:04:38 -0600

    Avance

--- UTesting/UTesting/Homework.cs
+++ UTesting/UTesting/Homework.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 
 namespace UTesting
@@ -9,12 +11,33 @@ namespace UTesting
     {
         public string ValidateEmail(string email)
         {
-            throw new NotImplementedException();
+            if (email.Count(c => c == '.') > 1)
+                return "no valid";
+
+
+            Regex rgx = new Regex("^\\w{1,12}[@]\\w{1,8}[.]{1}(com|net|gov|mx|microsoft)");
+            if (rgx.IsMatch(email.ToLower()))
+            {
+                return "valid";
+            }
+            else {
+
+                return "no valid";
+            }
+
         }
 
         public string ValidateFibonnacci(string list)
         {
-            throw new NotImplementedException();
+            Regex rgx = new Regex("^$|^(^1|(?>\\2?)(\\1))*1$");
+
+
+            string a =
+
+            //if (rgx.IsMatch(list))
+            //    return "valid";
+            //else
+            //    return "no valid";
         }
 
         public string ValidatePassword(string password)
@@ -24,12 +47,40 @@ namespace UTesting
 
         public string ValidatePhone(string phone)
         {
-            throw new NotImplementedException();
+            Regex rgx = new Regex("^\\d{2,4}[- ]?\\s?\\d{1,3}[- ]?\\s?\\d{1,3}[- ]?\\s?\\d{1,3}[- ]?\\s?");
+
+            if (rgx.IsMatch(phone))
+                return "valid";
+            else
+                return "no valid";
         }
 
         public string ValidateTime(string time)
         {
-            throw new NotImplementedException();
+            TimeSpan temp;
+
+            if (TimeSpan.TryParse(time, out temp))
+                return "valid";
+            else
+                return "no valid";
         }
+
+
+        //public static int Fibonacci(int n)
+        //{
+            
+        //   int a = 0;
+        //   int  b = 1; 
+        //    for (i = 0; i < n; i++)  //se repite para el número de veces deseado
+        //    {
+        //        aux = a;
+        //        a = b; //intercambio valores
+        //        b = aux + a; //cailculo el nuevo valor
+        //        Console.WriteLine(aux); //imprimo el valor
+        //    }
+        //}
+
+      
+
     }
 }
